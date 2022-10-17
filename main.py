d2847afe255ded5f32593b8b6dedd45506d3c04b
icechocolatee <park00099@naver.com>
2022-10-18 01:38:57 +0900

    20221017 (2)

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-printf("계산할 첫번째 정수를 입력해주세요.")
+print("첫번째 숫자 입력 : ")
 num1 = int(input())
 
-printf("계산할 두번째 정수를 입력해주세요.")
+print("두번째 숫자 입력 : ")
 num2 = int(input())
 
-printf("1 = 더하기")
-printf("2 = 빼기")
-printf("3 = 곱하기")
-printf("4 = 나누기")
-printf("계산할 사칙연산을 선택해주세요.")
+print("1 = 더하기")
+print("2 = 빼기")
+print("3 = 곱하기")
+print("4 = 나누기")
+print("계산 입력 ( +, -, *, / ) : ")
 symbol = int(input())
 
 if symbol == 1:
+    print(" ## 계산기 : ")
     print(str(num1) + " + " + str(num2) + " = " + str(num1 + num2))
 if symbol == 2:
+    print(" ## 계산기 : ")
     print(str(num1) + " - " + str(num2) + " = " + str(num1 - num2))
 if symbol == 3:
+    print(" ## 계산기 : ")
     print(str(num1) + " * " + str(num2) + " = " + str(num1 * num2))
 if symbol == 4:
+    print(" ## 계산기 : ")
     print(str(num1) + " / " + str(num2) + " = " + str(num1 / num2))
 else:
     print("정확한 사칙연산 선택이 아닙니다. 프로그램을 종료합니다.")
