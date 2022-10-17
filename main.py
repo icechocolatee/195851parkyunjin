ec545d7dfb79bf7118bdbe88ee37060f65d5355a
icechocolatee <park00099@naver.com>
2022-10-18 00:39:09 +0900

    20221017 (1)

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+printf("계산할 첫번째 정수를 입력해주세요.")
+num1 = int(input())
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+printf("계산할 두번째 정수를 입력해주세요.")
+num2 = int(input())
 
+printf("1 = 더하기")
+printf("2 = 빼기")
+printf("3 = 곱하기")
+printf("4 = 나누기")
+printf("계산할 사칙연산을 선택해주세요.")
+symbol = int(input())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if symbol == 1:
+    print(str(num1) + " + " + str(num2) + " = " + str(num1 + num2))
+if symbol == 2:
+    print(str(num1) + " - " + str(num2) + " = " + str(num1 - num2))
+if symbol == 3:
+    print(str(num1) + " * " + str(num2) + " = " + str(num1 * num2))
+if symbol == 4:
+    print(str(num1) + " / " + str(num2) + " = " + str(num1 / num2))
+else:
+    print("정확한 사칙연산 선택이 아닙니다. 프로그램을 종료합니다.")
